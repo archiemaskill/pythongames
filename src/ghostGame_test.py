1d89e23f8c06a443e0e6ea343632257544253dd7
Archie Maskill <archie.maskill@ndph.ox.ac.uk>
2023-12-03 22:10:21 +0000

    ghostGame test now inside a unittest.TestCase

--- src/ghostGame_test.py
+++ src/ghostGame_test.py
@@ -1,17 +1,22 @@
+import unittest
+
 from ghostGame import GhostGame
 from src.stdout_printer import StdOutPrinter
 
 
-def test_prints_welcome_message():
-	printer = StdOutPrinter.create_null()
-	game = GhostGame(printer)
-	tracker = printer.track_output()
+class GameTest(unittest.TestCase):
+	@staticmethod
+	def test_prints_welcome_message():
+		printer = StdOutPrinter.create_null()
+		game = GhostGame(printer)
+		tracker = printer.track_output()
 
-	game.start()
+		game.start()
 
-	assert tracker.output() == [
-		"Welcome to Ghost Game!",
-		"There are 3 doors ahead, a ghost is behind one door",
-		"You keep on choosing a door until it is a ghost door",
-		"Each door without a ghost gets you 5 points",
-		"Which door: 1, 2, or 3?"]
+		assert tracker.output() == [
+			"Welcome to Ghost Game!",
+			"There are 3 doors ahead, a ghost is behind one door",
+			"You keep on choosing a door until it is a ghost door",
+			"Each door without a ghost gets you 5 points",
+			"Which door: 1, 2, or 3?"]
+

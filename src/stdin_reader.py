2983668e6d2a7f75434b8e5f9aad7821937d32a6
Archie Maskill <archie.maskill@ndph.ox.ac.uk>
2023-12-03 10:13:32 +0000

    Nulled StdInReader can be configured with some lines to return

--- src/stdin_reader.py
+++ src/stdin_reader.py
@@ -1,9 +1,19 @@
 class StdInReader:
-	@staticmethod
-	def create_null():
-		instance = StdInReader()
-		return instance
+
+	def __init__(self, lines):
+		self.lines = lines
+		self.current = 0
+		pass
 
 	@staticmethod
-	def read_line():
-		return "1"
+	def create_null(*lines):
+		instance = StdInReader(lines)
+		return instance
+
+	def read_line(self):
+		if self.lines:
+			item = self.lines[self.current]
+			self.current += 1
+			return item
+		else:
+			return "1"
--- src/stdin_reader_test.py
+++ src/stdin_reader_test.py
@@ -8,3 +8,10 @@ def test_non_configured_always_returns_1():
 	assert reader.read_line() == "1"
 	assert reader.read_line() == "1"
 
+
+def test_can_be_configured():
+	reader = StdInReader.create_null("1", "2", "3")
+
+	assert reader.read_line() == "1"
+	assert reader.read_line() == "2"
+	assert reader.read_line() == "3"

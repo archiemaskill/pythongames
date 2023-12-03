e194ea9a020d13e0a43030b905ee77768661323d
Archie Maskill <archie.maskill@ndph.ox.ac.uk>
2023-12-03 12:40:26 +0000

    StdInReader: the reader reads from stdin

--- src/stdin_reader.py
+++ src/stdin_reader.py
@@ -1,16 +1,32 @@
 class StdInReader:
 
-	def __init__(self, lines):
-		self.lines = lines
-		self.current = 0
-		pass
+	@staticmethod
+	def create():
+		instance = StdInReader("", False)
+		return instance
 
 	@staticmethod
 	def create_null(*lines):
-		instance = StdInReader(lines)
+		instance = StdInReader(lines, True)
 		return instance
 
+	def __init__(self, lines, is_nulled):
+		self.lines = lines
+		self.current = 0
+		self.is_nulled = is_nulled
+		pass
+
 	def read_line(self):
+		if self.is_nulled:
+			return self.stubbed_input()
+		else:
+			return self.real_input()
+
+	@staticmethod
+	def real_input():
+		return input()
+
+	def stubbed_input(self):
 		if self.lines:
 			if self.current >= len(self.lines):
 				raise StopIteration("No more lines configured in nulled StdInReader")
--- src/stdin_reader_test.py
+++ src/stdin_reader_test.py
@@ -1,25 +1,37 @@
 import unittest
+from io import StringIO
 
-from src.stdin_reader import StdInReader
-
-
-def test_non_configured_always_returns_1():
-	reader = StdInReader.create_null()
-
-	assert reader.read_line() == "1"
-	assert reader.read_line() == "1"
-	assert reader.read_line() == "1"
+from stdin_reader import StdInReader
+from unittest.mock import patch
 
 
-def test_can_be_configured():
-	reader = StdInReader.create_null("1", "2", "3")
+class Reader(unittest.TestCase):
 
-	assert reader.read_line() == "1"
-	assert reader.read_line() == "2"
-	assert reader.read_line() == "3"
+	@patch("sys.stdin", StringIO("first line\nsecond line\n\n"))
+	def test_reads_user_input_from_stdin(self):
+		reader = StdInReader.create()
+
+		self.assertEqual(reader.read_line(), "first line")
+		self.assertEqual(reader.read_line(), "second line")
+		self.assertEqual(reader.read_line(), "")
 
 
-class MyTestClass(unittest.TestCase):
+class NulledReader(unittest.TestCase):
+
+	def test_non_configured_always_returns_the_default_value(self):
+		reader = StdInReader.create_null()
+
+		self.assertEqual(reader.read_line(), "1")
+		self.assertEqual(reader.read_line(), "1")
+		self.assertEqual(reader.read_line(), "1")
+
+	def test_can_be_configured(self):
+		reader = StdInReader.create_null("1", "2", "3")
+
+		self.assertEqual(reader.read_line(), "1")
+		self.assertEqual(reader.read_line(), "2")
+		self.assertEqual(reader.read_line(), "3")
+
 	def test_raises_exception_when_runs_out_of_configured_values(self):
 		reader = StdInReader.create_null("1")
 		reader.read_line()
